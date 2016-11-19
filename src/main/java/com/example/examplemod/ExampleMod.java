5bcfc6a5697a0243f33faddcfe02aa905ae92149
ttsutchi <tailegog@gmail.com>
2016-11-20 00:09:52 +0900

    Fix in RenderCube.java and create 'registerCube()' in ExampleMod.java

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
@@ -13,11 +14,14 @@ public class ExampleMod
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
 
-    public static final ItemCube itemCube = new ItemCube();
+    public static final Item itemCube = new ItemCube();
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
+        registerCube();
+    }
+
+    public void registerCube() {
         GameRegistry.registerItem(itemCube, "itemCube");
         EntityRegistry.registerModEntity(EntityCube.class, "monsterball", EntityRegistry.findGlobalUniqueEntityId(), this, 10, 10, true);
         RenderingRegistry.registerEntityRenderingHandler(EntityCube.class, new RenderCube(itemCube));
--- src/main/java/com/example/examplemod/RenderCube.java
+++ src/main/java/com/example/examplemod/RenderCube.java
@@ -4,32 +4,47 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.projectile.EntityPotion;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemPotion;
+import net.minecraft.potion.PotionHelper;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 
 public class RenderCube extends Render {
-    private ItemCube item;
+    private Item item;
 
-    public RenderCube(ItemCube item) {
+    public RenderCube(Item item) {
         this.item = item;
     }
 
     @Override
-    public void doRender(Entity entity, double x, double y, double z, float width, float height) {
-        // テクスチャを取得する
-        IIcon icon = item.getIconForEntity((EntityCube) entity);
+    public void doRender(Entity p_76986_1_, double p_76986_2_, double p_76986_4_, double p_76986_6_, float p_76986_8_, float p_76986_9_) {
+        IIcon iicon = this.item.getIconFromDamage(p_76986_1_.getDataWatcher().getWatchableObjectByte(8));
 
-        if (icon != null) {
+        if (iicon != null) {
             GL11.glPushMatrix();
-            GL11.glTranslatef((float) x, (float) y, (float) z);
+            GL11.glTranslatef((float) p_76986_2_, (float) p_76986_4_, (float) p_76986_6_);
             GL11.glEnable(GL12.GL_RESCALE_NORMAL);
             GL11.glScalef(0.5F, 0.5F, 0.5F);
-            bindEntityTexture(entity);
+            this.bindEntityTexture(p_76986_1_);
             Tessellator tessellator = Tessellator.instance;
 
-            drawIcon(tessellator, icon);
+            if (iicon == ItemPotion.func_94589_d("bottle_splash")) {
+                int i = PotionHelper.func_77915_a(((EntityPotion) p_76986_1_).getPotionDamage(), false);
+                float f2 = (float) (i >> 16 & 255) / 255.0F;
+                float f3 = (float) (i >> 8 & 255) / 255.0F;
+                float f4 = (float) (i & 255) / 255.0F;
+                GL11.glColor3f(f2, f3, f4);
+                GL11.glPushMatrix();
+                this.drawIcon(tessellator, ItemPotion.func_94589_d("overlay"));
+                GL11.glPopMatrix();
+                GL11.glColor3f(1.0F, 1.0F, 1.0F);
+            }
+
+            this.drawIcon(tessellator, iicon);
             GL11.glDisable(GL12.GL_RESCALE_NORMAL);
             GL11.glPopMatrix();
         }
