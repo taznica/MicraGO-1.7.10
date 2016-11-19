e383050ec0a3c70785018a843d24a2be10c85522
ttsutchi <tailegog@gmail.com>
2016-11-20 00:01:13 +0900

    Register 3 cube classes in ExampleMod.java

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,20 +1,25 @@
 package com.example.examplemod;
 
-import net.minecraft.init.Blocks;
+import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.EntityRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
-    
+
+    public static final ItemCube itemCube = new ItemCube();
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        GameRegistry.registerItem(itemCube, "itemCube");
+        EntityRegistry.registerModEntity(EntityCube.class, "monsterball", EntityRegistry.findGlobalUniqueEntityId(), this, 10, 10, true);
+        RenderingRegistry.registerEntityRenderingHandler(EntityCube.class, new RenderCube(itemCube));
     }
 }
