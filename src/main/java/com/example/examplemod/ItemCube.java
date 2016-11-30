b96ec363b67df849b47a7426bc1cfb7adb929466
ttsutchi <tailegog@gmail.com>
2016-11-30 23:18:41 +0900

    Add sub-item methods in ItemCube.java

--- src/main/java/com/example/examplemod/ItemCube.java
+++ src/main/java/com/example/examplemod/ItemCube.java
@@ -1,17 +1,28 @@
 package com.example.examplemod;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
+import java.util.List;
+
 public class ItemCube extends Item {
+
+    private IIcon[] iIcon = new IIcon[3];
+
     public ItemCube() {
         super();
         setUnlocalizedName(ExampleMod.MODID + "_itemCube");
         setTextureName(ExampleMod.MODID + ":itemCube");
         setCreativeTab(ExampleMod.tabGo);
+        setHasSubtypes(true);
+        setMaxDamage(3);
     }
 
     @Override
@@ -34,5 +45,37 @@ public class ItemCube extends Item {
         // アイテムと同じアイコンを使う
         // 別のテクスチャを使う場合はregisterIconsで登録したアイコンを返す
         return getIconFromDamage(0);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iIconRegister) {
+        for(int i = 0; i < 3; i++) {
+            this.iIcon[i] = iIconRegister.registerIcon(this.getIconString()+ "_i");
+        }
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIconFromDamage(int meta) {
+        return iIcon[meta];
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void getSubItems(Item item, CreativeTabs creativeTabs, List list) {
+        for(int i = 0; i < 3; i++) {
+            list.add(new ItemStack(this, 1, i));
+        }
+    }
+
+    @Override
+    public int getMetadata(int meta) {
+        return meta;
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemStack) {
+        return super.getUnlocalizedName() + "_" + itemStack.getItemDamage();
     }
 }
